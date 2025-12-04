6b8786585b914156924db503d5d3c0468576974c
gallegol0322-eng <gallego.l0322@gmail.com>
2025-12-04 09:40:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def process_confirmation(msg, state):
 
         if field == "ciudad":
            state["last_action"] = "save_phone"
-           return f"{state['name']} ¿tu número de WhatsApp?"
+           return f"{state['name']} regalame porfavor tu numero de telefono de esta forma: (mi numero es 3141234567)"
 
 
         if field == "telefono":
@@ -256,7 +256,7 @@ def process_confirmation(msg, state):
 
         if field == "telefono":
             state["last_action"] = "save_phone"
-            return "Ok, escríbeme de nuevo tu número de WhatsApp."
+            return "Ok, escríbeme de nuevo tu número de WhatsAp."
 
         return f"Ok, repíteme tu {field}."
 
@@ -396,6 +396,7 @@ def home():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
+
 
 
 
